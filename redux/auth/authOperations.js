82232646286d7120e420494eee93ef2e9f644f0c
Ihor Borkaliuk <happyviolence1990@gmail.com>
2022-12-16 22:34:02 +0100

    added display name

--- redux/auth/authOperations.js
+++ redux/auth/authOperations.js
@@ -1,13 +1,22 @@
 import "firebase/auth";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase/config";
+import { getAuth, updateProfile } from "firebase/auth";
 
 
 export const authSignUpUser =
-  ({ email, password }) =>
+  ({ email, password, login }) =>
   async () => {
     try {
+      const auth = getAuth();
+      
       const user = await createUserWithEmailAndPassword(auth, email, password);
+
+
+      updateProfile(auth.currentUser, {
+        displayName: login,
+      })
+
       console.log("user", user);
     } catch (error) {
       console.log("error", error);
